290ed18c1380bd38872d75ccc5e913e152649de8
16vaishu <vaishaliag1610@gmail.com>
2025-10-11 17:28:12 +0530

    some changes

--- database.py
+++ database.py
@@ -16,5 +16,6 @@ POSTGRES_DB = os.getenv("POSTGRES_DB", "pysqlfastapi")
 SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+print(" Connected to:", SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
